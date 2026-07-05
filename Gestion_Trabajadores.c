ec16e93e8f2816174009831c71c722eb9ee230e8
Jose Ortega <jose.ortega.duque@udla.edu.ec>
2026-07-05 14:37:51 -0500

    diseño de estructuras, arreglos y validaciones

---
+++ Gestion_Trabajadores.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <string.h>
+#include "empleados.h"
+float calcularSueldoTotal(Empleado e)
+{
+    float sueldoTotal = e.sueldoBase + (e.horasExtra * 3.02); // asumiendo $3.02 por hora extra
+    return sueldoTotal;
+}
+//funcion para listar empleados
+void listarEmpleados(Empleado empleados[], int cantidad)
+{
+    printf("Listado de Empleados:\n");
+    printf("Codigo\tNombre\tCargo\tSueldo Base\tHoras Extra\tSueldo Total\n");
+    for(int i = 0; i < cantidad; i++)
+    {
+        float sueldoTotal = calcularSueldoTotal(empleados[i]);
+        printf("%s\t%s\t%s\t%.2f\t%d\t%.2f\n", empleados[i].codigo, empleados[i].nombre, empleados[i].cargo, empleados[i].sueldoBase, empleados[i].horasExtra, sueldoTotal);
+    }
+}
+//eliminar empleado
+int eliminarEmpleado(Empleado empleados[], int *cantidad, char codigo[])
+{
+    for(int i = 0; i < *cantidad; i++)
+    {
+        if(strcmp(empleados[i].codigo, codigo) == 0)
+        {
+            for(int j = i; j < *cantidad - 1; j++)
+            {
+                empleados[j] = empleados[j + 1];
+            }
+            (*cantidad)--;
+            return 1; // empleado eliminado
+        }
+    }
+    return 0; 
+}
--- empleados.h
+++ empleados.h
@@ -17,4 +17,15 @@ void listarEmpleados(Empleado empleados[], int cantidad);
 
 int eliminarEmpleado(Empleado empleados[], int *cantidad, char codigo[]);
 
+
+void menuPrincipal(Empleado empleados[], int *cantidad);
+void registrarEmpleado(Empleado empleados[], int *cantidad);
+void buscarEmpleado(Empleado empleados[], int cantidad);
+void actualizarEmpleado(Empleado empleados[], int cantidad);
+
+// Funciones para cumplir con el requisito de "Validaciones"
+void limpiarSaltoLinea(char *cadena);
+int esCodigoUnico(Empleado empleados[], int cantidad, const char *codigo);
+int esAlfanumericoSinEspacios(const char *cadena);
+
 #endif
